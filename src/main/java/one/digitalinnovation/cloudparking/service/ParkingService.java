4f7744c40fa4b7f53c98a6f7b3053ff18c7b1f46
TaisisMarinelo <taisismarinelo@gmail.com>
2023-09-13 21:05:54 -0300

    get-post

--- src/main/java/one/digitalinnovation/cloudparking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/cloudparking/service/ParkingService.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.cloudparking.service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,8 +19,11 @@ public class ParkingService {
 	
 	static {
 		var id1 = getUUID();
+		var id2 = getUUID();
 		Parking parking = new Parking(id1, "DMS-1111", "SC", "CELTA", "Preto");
+		Parking parking2 = new Parking(id2, "DMS-2222", "RJ", "CELTA", "Preto");
 		parkingMap.put(id1, parking);
+		parkingMap.put(id2, parking2);
 
 	}
 	
@@ -29,6 +33,18 @@ public class ParkingService {
 	
 	private static String getUUID() {
 		return UUID.randomUUID().toString().replace("-", "");
+	}
+
+	public Parking findById(String id) {
+		return parkingMap.get(id);
+	}
+
+	public Parking create(Parking parkingCreate) {
+		String uuid = getUUID();
+		parkingCreate.setId(uuid);
+		parkingCreate.setEntryDate(LocalDateTime.now());
+		parkingMap.put(uuid, parkingCreate);
+		return parkingCreate;
 	}
 	
 }
