7cde7c90b5b25090b8e7920bf7b7fcd3bdb517f9
TaisisMarinelo <taisismarinelo@gmail.com>
2023-09-25 21:21:03 -0300

    correcaoTeste-dependenciasJpa

--- src/test/java/one/digitalinnovation/cloudparking/controllerIT/ParkingControllerIT.java
+++ src/test/java/one/digitalinnovation/cloudparking/controllerIT/ParkingControllerIT.java
@@ -1,5 +1,9 @@
 package one.digitalinnovation.cloudparking.controllerIT;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,12 +13,31 @@ import org.springframework.http.MediaType;
 
 import io.restassured.RestAssured;
 import one.digitalinnovation.cloudparking.controller.dto.ParkingCreateDTO;
+import one.digitalinnovation.cloudparking.model.Parking;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT )
 class ParkingControllerIT {
 	
 	@LocalServerPort
 	private int randomPort; 	//criando porta aleatoria
+	
+	
+	private static String getUUID() {
+		return UUID.randomUUID().toString().replace("-", "");
+	}
+	
+	//List de dados mokados
+	private static Map<String, Parking> parkingMap = new HashMap<>();
+	
+	static {
+		var id1 = getUUID();
+		var id2 = getUUID();
+		Parking parking = new Parking(id1, "DMS-1111", "SC", "CELTA", "Preto");
+		Parking parking2 = new Parking(id2, "DMS-2222", "RJ", "CELTA", "Preto");
+		parkingMap.put(id1, parking);
+		parkingMap.put(id2, parking2);
+
+	}
 	
 	@BeforeEach
 	public void setUpteste() {
@@ -28,7 +51,7 @@ class ParkingControllerIT {
 			.get("/parking")
 			.then()
 			.statusCode(200)
-			.body("license[0]", Matchers.equalTo("DMS-1111"));
+			.body("license[1]", Matchers.equalTo("DMS-1111"));
 	}
 	
 	@Test
